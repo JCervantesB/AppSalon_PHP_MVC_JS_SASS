9054816469c672aec1ce1f05da93302248888d58
JCervantesB <pc3dtepic@gmail.com>
2021-11-02 18:00:36 -0700

    Creando el CRUD Servicios

--- models/Servicio.php
+++ models/Servicio.php
@@ -16,4 +16,15 @@ class Servicio extends ActiveRecord {
         $this->nombre = $args['nombre'] ?? '';
         $this->precio = $args['precio'] ?? '';
     }
+
+    public function validar() {
+        if(!$this->nombre) {
+            self::$alertas['error'][] = 'El Nombre del Servicio es Obligatorio';
+        }
+        if(!is_numeric($this->precio)) {
+            self::$alertas['error'][] = 'El precio no es valido';
+        }
+
+        return self::$alertas;
+    }
 }
--- views/templates/barra.php
+++ views/templates/barra.php
@@ -5,3 +5,11 @@
     <p><?php echo $nombre ?? '';?></p>
     <a class="boton" href="/logout">Cerrar Sessión</a>
 </div>
+
+<?php if(isset($_SESSION['admin'])) { ?>
+    <div class="barra-servicios">
+        <a href="/admin" class="boton">Ver Citas</a>
+        <a href="/servicios" class="boton">Ver Servicios</a>
+        <a href="/servicios/crear" class="boton">Crear Servicio</a>
+    </div>
+<?php } ?>
