8fb5a4d9628e558217ef9d755f238ce5fb3f925c
JCervantesB <imjcervantes@gmail.com>
2022-02-11 10:15:54 -0700

    Test: error de rutas

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -14,9 +14,9 @@ class AdminController {
         $fecha = $_GET['fecha'] ?? date('Y-m-d');
         $fechas = explode('-', $fecha);
         
-        // if(!checkdate($fechas[1], $fechas[2], $fechas[0])) {
-        //     header('Location: /404');
-        // }
+        if(!checkdate($fechas[1], $fechas[2], $fechas[0])) {
+            header('Location: /404');
+        }
         
         // Consultar la base de datos
         $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
@@ -28,7 +28,7 @@ class AdminController {
         $consulta .= " ON citasservicios.citaId=citas.id ";
         $consulta .= " LEFT OUTER JOIN servicios ";
         $consulta .= " ON servicios.id=citasservicios.servicioId ";
-        $consulta .= " WHERE fecha =  '${fecha}' ";
+        $consulta .= " WHERE fecha =  '2022-02-14' ";
 
         $citas = AdminCita::SQL($consulta);
        
