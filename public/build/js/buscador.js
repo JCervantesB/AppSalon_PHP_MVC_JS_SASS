9528fc1eaaac55ff0698e2ad57e217bbcca3599c
JCervantesB <imjcervantes@gmail.com>
2022-02-11 10:10:11 -0700

    fix buscador.js

--- public/build/js/buscador.js
+++ public/build/js/buscador.js
@@ -1,1 +1,1 @@
-function iniciarApp(){buscarPorFecha()}function buscarPorFecha(){document.querySelector("#fecha").addEventListener("input",(function(n){const e=n.target.value;window.location="?fecha="+e}))}document.addEventListener("DOMContentLoaded",(function(){iniciarApp()}));
+function iniciarApp(){buscarPorFecha()}function buscarPorFecha(){document.querySelector("#fecha").addEventListener("input",(function(n){const e=n.target.value;window.location=e?"admin?fecha="+e:"admin"}))}document.addEventListener("DOMContentLoaded",(function(){iniciarApp()}));
